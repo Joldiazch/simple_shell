327f85592ece54fc6c581707b018bfeb710f149d
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-27 17:32:33 -0500

    modificate _read line function

--- _read_line.c
+++ _read_line.c
@@ -9,8 +9,8 @@ char *_read_line(ssize_t *c, char **paths, list_t *head)
 {
 	char *read = NULL;
 	size_t buffsize = 0;
-
-	write(STDOUT_FILENO, "MiShellaDa $: ", 15);
+	if (isatty(STDIN_FILENO) == 1)
+		write(STDOUT_FILENO, "MiShellaDa $: ", 15);
 	/* read = calloc(size * sizeof(char), sizeof(char)); */
 	*c = getline(&read, &buffsize, stdin);
 	if (*c == EOF)
