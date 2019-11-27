7101deb2c84d16e77f15d6622651f0d1c454423b
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-27 16:38:45 -0500

    modificate number of lines in main

--- main2.c
+++ main2.c
@@ -10,10 +10,12 @@
 int main(__attribute__((unused))int gc, char **argv, char **env)
 {
 	char **paths, *buff, *search_paths, del[7] = " \t\r\n\a";
-	pid_t child; int status; ssize_t c = 0; list_t *head;
+	pid_t child;
+	int status;
+	list_t *head;
+	ssize_t c = 0;
 
-	argv = NULL;
-	head = NULL;
+	argv = head = NULL;
 	search_paths = _getenv("PATH", env);
 	paths = _tokenizar(search_paths, ":");
 	build_list(paths, &head);
@@ -36,24 +38,15 @@ int main(__attribute__((unused))int gc, char **argv, char **env)
 		argv[0] = _concatenar(&head, argv[0]);
 		child = fork();
 		if (child == -1)
-		{
 			perror("unborn child");
-		}
 		else if (child == 0)
 		{
 			if (argv[0] && (execve(argv[0], argv, NULL) == -1))
-			{
 				perror("Mishellada");
-			}
 			exit(EXIT_FAILURE);
 		}
 		else
-		{
 			wait(&status);
-			/* free(argv[0]);
-			   free(buff);
-			   free(argv); */
-		}
 		freeLoop(argv[0], buff, argv);
 	}
 	return (0);
